9e0a209d0d4378b4df5f48752ad2bcee204386a5
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-03 20:19:32 -0800

    backing up progress

--- firmware/shared/ruckus.tcl
+++ firmware/shared/ruckus.tcl
@@ -10,3 +10,10 @@ loadSource -dir "$::DIR_PATH/rtl"
 
 # Load IP cores
 loadIpCore -dir "$::DIR_PATH/ip"
+
+# Load SYSGEN .ZIP output file
+if { [get_ips analysis_0] eq ""  } {
+   loadZipIpCore  -repo_path $::env(IP_REPO) -dir "$::DIR_PATH/simulink/netlist/ip"
+   create_ip -name analysis -vendor SLAC -library SysGen -version 1.0 -module_name analysis_0
+   set_property target_language Verilog [current_project]
+}
--- firmware/targets/SimpleZcu216Example/vivado/post_synthesis.tcl
+++ firmware/targets/SimpleZcu216Example/vivado/post_synthesis.tcl
@@ -38,21 +38,57 @@ CreateDebugCore ${ilaName}
 #######################
 ## Set the record depth
 #######################
-set_property C_DATA_DEPTH 1024 [get_debug_cores ${ilaName}]
+set_property C_DATA_DEPTH 8192 [get_debug_cores ${ilaName}]
 
 #################################
 ## Set the clock for the ILA core
 #################################
-SetDebugCoreClk ${ilaName} {U_Core/REAL_CPU.U_CPU/U_CPU/dmaClk}
+SetDebugCoreClk ${ilaName} {U_Hardware/U_I2C_CLK104/axilClk}
 
 #######################
 ## Set the debug Probes
 #######################
 
-ConfigProbe ${ilaName} {U_Core/REAL_CPU.U_CPU/U_CPU/dmaRstL}
-ConfigProbe ${ilaName} {U_Core/REAL_CPU.U_CPU/regRead*}
-ConfigProbe ${ilaName} {U_Core/REAL_CPU.U_CPU/regWrite*}
-ConfigProbe ${ilaName} {U_Core/REAL_CPU.U_CPU/U_CPU/zynq_ultra_ps_e_0_M_AXI_HPM0_FPD*}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilReadMaster[araddr][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilWriteMaster[awaddr][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilWriteMaster[wdata][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilReadSlave][rdata][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilReadSlave][rresp][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilWriteSlave][bresp][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][i2cAddr][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regAddr][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regAddrSize][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regDataSize][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regWrData][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[sdoMuxSel][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[state][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/regOut[regFailCode][*]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/regOut[regRdData][*]}
+
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilReadMaster[arvalid]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilReadMaster[rready]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilRst}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilWriteMaster[awvalid]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilWriteMaster[bready]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/axilWriteMaster[wvalid]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/i2co[scloen]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/i2co[sdaoen]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilReadSlave][arready]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilReadSlave][rvalid]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilWriteSlave][awready]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilWriteSlave][bvalid]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[axilWriteSlave][wready]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][busReq]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][endianness]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regAddrSkip]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regOp]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][regReq]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][repeatStart]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/r[regIn][tenbit]}
+# ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/readEnable}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/regOut[regAck]}
+ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/regOut[regFail]}
+# ConfigProbe ${ilaName} {U_Hardware/U_I2C_CLK104/writeEnable}
 
 ##########################
 ## Write the port map file
