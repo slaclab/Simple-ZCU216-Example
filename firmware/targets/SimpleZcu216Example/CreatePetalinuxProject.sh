2f090469d5324bebd175224b020ca126221d8148
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-06 07:53:48 -0800

    upgrading to rogue v5.13.0 and adding more user args

--- firmware/targets/SimpleZcu216Example/CreatePetalinuxProject.sh
+++ firmware/targets/SimpleZcu216Example/CreatePetalinuxProject.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+####################################################
+
+# Define the hardware type
+# Note: Must match the axi-soc-ultra-plus-core/hardware directory name
+hwType=XilinxZcu216
+
+# Define number of DMA lanes
+numLane=2
+
+# Define number of DEST per DMA lane
+numDest=32
+
+# Define number of DMA TX/RX Buffers
+rxBuffCnt=256
+txBuffCnt=16
+
+# Define DMA Buffer Size
+buffSize=0x200000 # 2MB
+
+####################################################
 
 if [ $# -ne 1 ]
 then
@@ -6,18 +26,19 @@ then
    exit;
 fi
 
-# Define the hardware type
-hwType=XilinxZcu216
+# Define the target name
+xsaPath=$(realpath "${1}")
+
+# Define the target name
+targetName=${PWD##*/}
 
 # Make the build output
 mkdir -p /u1/$USER
 mkdir -p /u1/$USER/build
 mkdir -p /u1/$USER/build/petalinux
+buildPath=/u1/$USER/build/petalinux
 
 # Execute the create petalinux script
 ../../submodules/axi-soc-ultra-plus-core/CreatePetalinuxProject.sh \
--p /u1/$USER/build/petalinux \
--n ${PWD##*/} \
--x $(realpath "${1}") \
--h $hwType \
--j 1
+-p $buildPath -n $targetName -x $xsaPath -h $hwType \
+-l $numLane -d $numDest -t $txBuffCnt -r $rxBuffCnt -s $buffSize
