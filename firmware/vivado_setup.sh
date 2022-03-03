86fe3979d4e7030858dfd12076b7d41cae07107a
Larry Ruckman <ruckman@slac.stanford.edu>
2022-03-03 12:56:31 -0800

    updating script

--- firmware/vivado_setup.sh
+++ firmware/vivado_setup.sh
@@ -1,2 +1,5 @@
 # Setup the Xilinx software and licensing
 source /afs/slac.stanford.edu/g/reseng/xilinx/2021.2/Vivado/2021.2/settings64.sh
+
+# Setup the petalinux software and licensing
+source /afs/slac.stanford.edu/g/reseng/petalinux/2021.2/settings.sh
