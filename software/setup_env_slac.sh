fa5f00e098c40213dfbab24724c2b787f44ce9d2
Larry Ruckman <ruckman@slac.stanford.edu>
2022-07-22 09:37:49 -0700

    SW update

--- software/setup_env_slac.sh
+++ software/setup_env_slac.sh
@@ -6,4 +6,4 @@ source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/co
 ##################################
 # Activate Rogue conda Environment
 ##################################
-conda activate rogue_v5.13.0
+conda activate rogue_v5.14.0
