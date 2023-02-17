2d931401d34154e4d993cbf02eae92828ecf40d0
Larry Ruckman <ruckman@slac.stanford.edu>
2023-02-16 16:09:11 -0800

    minor updates

--- firmware/sysgen_setup.sh
+++
@@ -1,8 +0,0 @@
-# Setup the Xilinx software and licensing
-#source /afs/slac.stanford.edu/g/reseng/xilinx/2019.1/Vivado/2019.1/settings64.sh
-source  /afs/slac/g/controls/development/firmware/setup/settings64_Vivado_2019.1.sh
-
-# Setup Matlab software and licensing
-#source /afs/slac/g/lcls/package/matlab/2019a/settings64.sh
-source  /afs/slac/g/controls/development/package/matlab/setup/matlab_2019a_setup_local.bash
-
