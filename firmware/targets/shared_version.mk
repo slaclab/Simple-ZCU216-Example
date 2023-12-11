79a414f3d2cba5d989cfa2120f9f9969ec02116b
Larry Ruckman <ruckman@slac.stanford.edu>
2023-12-11 11:02:52 -0800

    Update shared_version.mk

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -1,6 +1,9 @@
 # Define Firmware Version: v1.8.0.0
 export PRJ_VERSION = 0x01080000
 
+# Include .XSA in image dir
+export GEN_XSA_IMAGE = 1
+
 # Define release
 ifndef RELEASE
 export RELEASE = all
