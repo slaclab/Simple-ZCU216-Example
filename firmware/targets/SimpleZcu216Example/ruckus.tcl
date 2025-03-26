a82ae66789d0e7c4e0655740ea4c844296ecbe36
Larry Ruckman <ruckman@slac.stanford.edu>
2025-03-26 10:24:53 -0700

    updating ruckus.tcl to be consistent
    updating remaining ruckus.tcl files to use the more portable version of loading proc

--- firmware/targets/SimpleZcu216Example/ruckus.tcl
+++ firmware/targets/SimpleZcu216Example/ruckus.tcl
@@ -1,5 +1,5 @@
 # Load RUCKUS environment and library
-source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+source $::env(RUCKUS_PROC_TCL)
 
 # Load common ruckus.tcl files
 loadRuckusTcl $::env(TOP_DIR)/shared
